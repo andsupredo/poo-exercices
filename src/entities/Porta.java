0ffc2bc432f502c0d6519787fcdd60c0ed28232e
Anderson Pedro <64421696+andsupredo@users.noreply.github.com>
2022-05-12 11:28:17 -0300

    Update Porta.java

--- src/entities/Porta.java
+++ src/entities/Porta.java
@@ -2,51 +2,51 @@ package entities;
 
 public class Porta {
 
-	public boolean estaAberta;
-	public String cor;
-	public double dimensaoX;
-	public double dimensaoY;
-	public double dimensaoZ;
-	
-	public Porta (boolean estaAberta, String cor, double dimensaoX, double dimensaoY, double dimensaoZ) {
-		this.estaAberta = estaAberta;
-		this.cor = cor;
-		this.dimensaoX = dimensaoX;
-		this.dimensaoY = dimensaoY;
-		this.dimensaoZ = dimensaoZ;
-	}
-	
-	public boolean abre() {
-		if (this.estaAberta == true) {
-			System.out.println("A porta j· est· aberta!");
-		}
-		else {
-			System.out.println("Abrindo porta...");
-		}
-		return this.estaAberta = true;
-	}
-	
-	public boolean fecha () {
-		if (this.estaAberta == false) {
-			System.out.println("A porta j· est· fechada!");
-		}
-		else {
-			System.out.println("Fechando porta...");
-		}
-		return this.estaAberta = false;
-	}
-	
-	public String pinta(String cor) {
-		System.out.println("pintando porta de " + cor + "...");
-		return cor;
-	}
-	
-	public String estaAberta() {
-		if (this.estaAberta == true) {
-			return "A porta est· aberta!";
-		}
-		else {
-			return "A porta est· fechada!";
-		}
-	}
+    public boolean estaAberta;
+    public String cor;
+    public double dimensaoX;
+    public double dimensaoY;
+    public double dimensaoZ;
+
+    public Porta (boolean estaAberta, String cor, double dimensaoX, double dimensaoY, double dimensaoZ) {
+        this.estaAberta = estaAberta;
+        this.cor = cor;
+        this.dimensaoX = dimensaoX;
+        this.dimensaoY = dimensaoY;
+        this.dimensaoZ = dimensaoZ;
+    }
+
+    public boolean abre() {
+        if (this.estaAberta) {
+            System.out.println("A porta j√° est√° aberta!");
+        }
+        else {
+            System.out.println("Abrindo porta...");
+        }
+        return this.estaAberta = true;
+    }
+
+    public boolean fecha() {
+        if (!this.estaAberta) {
+            System.out.println("A porta j√° est√° fechada!");
+        }
+        else {
+            System.out.println("Fechando porta...");
+        }
+        return this.estaAberta = false;
+    }
+
+    public String pinta(String cor) {
+        System.out.println("pintando porta de " + cor + "...");
+        return cor;
+    }
+
+    public String estaAberta() {
+        if (this.estaAberta) {
+            return "A porta est√° aberta!";
+        }
+        else {
+            return "A porta est√° fechada!";
+        }
+    }
 }
